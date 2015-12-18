bae5677ac5bec1baba0e902d37ee80001cee83d8
Dirk Gadsden <dirk@esherido.com>
2015-12-17 21:05:15 -0800

    Add trivial test and partial equivalence traits

--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -1,14 +1,28 @@
 #![allow(dead_code)]
 
-struct Program {
+pub struct Program {
     stmts: Vec<Statement>
 }
 
+impl Program {
+    fn new() -> Program {
+        Program {
+            stmts: Vec::new(),
+        }
+    }
+
+    fn push_mod(&mut self, m: Mod) {
+        self.stmts.push(Statement::StatementMod(m));
+    }
+}
+
+#[derive(PartialEq)]
 struct BasicBlock {
     stmts: Vec<Statement>
 }
 
-enum Statement {
+#[derive(PartialEq)]
+pub enum Statement {
     StatementMod(Mod),
     StatementExtern(Extern),
     StatementConst(Const),
@@ -26,75 +40,117 @@ enum Statement {
     StatementBreak,
 }
 
-type Name = String;
+pub type Name = String;
 
-struct Path {
+#[derive(PartialEq)]
+pub struct Path {
     segments: Vec<Name>,
 }
 
-struct Mod {
+impl Path {
+    fn with_name(name: Name) -> Path {
+        Path { segments: vec![name], }
+    }
+}
+
+#[derive(PartialEq)]
+pub struct Mod {
     path: Path,
 }
 
+impl Mod {
+    fn new(path: Path) -> Mod {
+        Mod { path: path }
+    }
+}
+
+#[derive(PartialEq)]
 struct Extern {
     path: Path,
 }
 
+#[derive(PartialEq)]
 struct Const {
     name: Name,
     constructor: Path,
     argument: Option<String>,
 }
 
+#[derive(PartialEq)]
 struct Static {
     name: Name,
 }
 
+#[derive(PartialEq)]
 struct Local {
     name: Name,
 }
 
+#[derive(PartialEq)]
 struct Fn {
     name: Name,
     parameters: Vec<Name>,
     body: BasicBlock,
 }
 
+#[derive(PartialEq)]
 struct Return {
     name: Option<Name>,
 }
 
+#[derive(PartialEq)]
 struct Call {
     name: Name,
     arguments: Vec<Name>,
 }
 
+#[derive(PartialEq)]
 struct Test {
     name: Name,
 }
 
+#[derive(PartialEq)]
 struct If {
     condition: BasicBlock,
     then_sibling: Then,
 }
 
+#[derive(PartialEq)]
 struct Then {
     body: BasicBlock,
     else_sibling: Option<Else>
 }
 
+#[derive(PartialEq)]
 struct Else {
     body: BasicBlock,
 }
 
+#[derive(PartialEq)]
 struct While {
     body: BasicBlock,
     // Some if this While is the lead and it's followed by a Do
     do_sibling: Option<Box<Do>>,
 }
 
+#[derive(PartialEq)]
 struct Do {
     body: BasicBlock,
     // Some if this Do is lead and it's followed by a While
     while_sibling: Option<Box<While>>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_program() {
+        let mut p = Program::new();
+        assert_eq!(p.stmts.len(), 0);
+
+        let m = Mod::new(Path::with_name("test".to_string()));
+        p.push_mod(m);
+        assert_eq!(p.stmts.len(), 1);
+    }
 }
