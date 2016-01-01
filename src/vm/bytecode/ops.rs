8e6adbf6bb7d3927de9b34ef7a33a660c42bb40b
Dirk Gadsden <dirk@esherido.com>
2015-12-31 17:31:09 -0800

    Code cleanup in bytecode ops

--- src/vm/bytecode/ops.rs
+++ src/vm/bytecode/ops.rs
@@ -23,21 +23,10 @@ fn reg_to_option(reg: Reg) -> Option<Reg> {
 
 /// Adds host-native read and write methods, ie. `read_hu64` ("read host unsigned 64").
 trait NativeEndianReadWriteExt: io::Read + ReadBytesExt {
-    fn read_hu8(&mut self) -> u8 {
-        self.read_u8().unwrap()
-    }
-
-    fn read_hu16(&mut self) -> u16 {
-        self.read_u16::<NativeEndian>().unwrap()
-    }
-
-    fn read_hu32(&mut self) -> u32 {
-        self.read_u32::<NativeEndian>().unwrap()
-    }
-
-    fn read_hu64(&mut self) -> u64 {
-        self.read_u64::<NativeEndian>().unwrap()
-    }
+    fn read_hu8(&mut self) -> u8   { self.read_u8().unwrap() }
+    fn read_hu16(&mut self) -> u16 { self.read_u16::<NativeEndian>().unwrap() }
+    fn read_hu32(&mut self) -> u32 { self.read_u32::<NativeEndian>().unwrap() }
+    fn read_hu64(&mut self) -> u64 { self.read_u64::<NativeEndian>().unwrap() }
 }
 // Add HostReadWriteExt to all readables
 impl<R: io::Read + ReadBytesExt> NativeEndianReadWriteExt for R {}
@@ -48,14 +37,9 @@ trait ReadWriteTypesExt: NativeEndianReadWriteExt {
     fn read_local(&mut self) -> u16 { self.read_hu16() }
 
     fn read_args(&mut self) -> Vec<u8> {
-        let num_args          = self.read_hu8();
-        let mut args: Vec<u8> = vec![];
+        let num_args = self.read_hu8();
 
-        for _ in 0..num_args {
-            args.push(self.read_reg())
-        }
-
-        args
+        (0..num_args).enumerate().map(|_| self.read_reg()).collect()
     }
 }
 impl<R: NativeEndianReadWriteExt> ReadWriteTypesExt for R {}
